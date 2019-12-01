ba517dcb0f67f97407a6ffe8d4f0bdb20a997839
Marcin Białoń <marcin@bialon.net>
2019-12-01 20:13:32 +0100

    Day 1, part 1

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,37 @@
+use std::io;
+
+fn calc(mass: u32) -> u32 {
+    (mass / 3) - 2
+}
+
+pub fn sum(r: impl io::BufRead) -> io::Result<u32> {
+    let mut fuel = 0;
+    for line in r.lines() {
+        let mass = line.unwrap().parse::<u32>().unwrap();
+        fuel += calc(mass);
+    }
+    Ok(fuel)
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn calc_examples() {
+        assert_eq!(calc(12), 2);
+        assert_eq!(calc(14), 2);
+        assert_eq!(calc(1969), 654);
+        assert_eq!(calc(100756), 33583);
+    }
+
+    #[test]
+    fn sum_examples() {
+        let b = "12
+14
+1969
+100756"
+            .as_bytes();
+        assert_eq!(sum(b).unwrap(), 2 + 2 + 654 + 33583);
     }
 }
---
+++ src/main.rs
@@ -0,0 +1,6 @@
+use adventofcode2019::sum;
+use std::io::stdin;
+
+fn main() {
+    println!("{}", sum(stdin().lock()).unwrap())
+}
